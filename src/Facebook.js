df608f3d21ab93da7894784e6e16b13424e78009
MikkeGF <n9pemi00students.oamk.fi>
2020-11-05 18:53:33 +0200

    facebook

--- src/Facebook.js
+++ src/Facebook.js
@@ -1,27 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import { FacebookProvider, Comments, EmbeddedPost } from 'react-facebook';
 
 const Wrapper = styled.div`
  display: flex;
  justify-content: center;
+ margin: 50px;
 `;
+
 
 const Facebook = () => {
     return (
         <>
-        <Wrapper>
-        <FacebookProvider appId="1832935643512194">
-            <EmbeddedPost href="https://www.facebook.com/DonaldTrump/posts/10165764490280725" />
-        </FacebookProvider>
-        </Wrapper>
+            <Wrapper>
+                <FacebookProvider appId="1832935643512194">
+                    <EmbeddedPost href="https://www.facebook.com/DonaldTrump/posts/10165764490280725" />
+                </FacebookProvider>
+            </Wrapper>
+
             <Wrapper>
                 <FacebookProvider appId="1832935643512194">
                     <Comments href="https://www.facebook.com/DonaldTrump/posts/10165764490280725" />
                 </FacebookProvider>
             </Wrapper>
         </>
-    )
+    );
 }
 
 export default Facebook
