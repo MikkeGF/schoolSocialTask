0667286ce02fbd887ec48d5b7950825cc1ae0528
MikkeGF <n9pemi00students.oamk.fi>
2020-11-06 12:01:26 +0200

    gridarea testejä

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 
 import Twitter from './Twitter';
 import Facebook from './Facebook';
+import GlobalStyles from './GlobalStyles';
 
 function App() {
   return (
     <div className="App">
     <Twitter />
     <Facebook />
+    <GlobalStyles />
     </div>
   );
 }
